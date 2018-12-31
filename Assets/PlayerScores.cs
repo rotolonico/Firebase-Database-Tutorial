eefbc42724b23cc93e184f626449b5bee393bc51
Domenico Rotolo <rotolonico@gmail.com>
2018-12-31 21:59:07 +0100

    first commit

---
+++ Assets/PlayerScores.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Proyecto26;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScores : MonoBehaviour
+{
+    public Text scoreText;
+    public InputField nameText;
+    
+    private System.Random random = new System.Random(); 
+
+    User user = new User();
+    
+    public static int playerScore;
+    public static string playerName;
+    
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerScore = random.Next(0, 101);
+        scoreText.text = "Score: " + playerScore;
+    }
+
+    public void OnSubmit()
+    {
+        playerName = nameText.text;
+        PostToDatabase();
+    }
+    
+    public void OnGetScore()
+    {
+        RetrieveFromDatabase();
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = "Score: " + user.userScore;
+    }
+
+    private void PostToDatabase()
+    {
+        User user = new User();
+        RestClient.Put("https://project-id.firebaseio.com/"+ playerName + ".json", user);
+    }
+
+    private void RetrieveFromDatabase()
+    {
+        RestClient.Get<User>("https://project-id.firebaseio.com/" + nameText.text + ".json").Then(response =>
+            {
+                user = response;
+                UpdateScore();
+            });
+    }
+}
---
+++ Assets/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class User
+{
+    public string userName;
+    public int userScore;
+
+    public User()
+    {
+        userName = PlayerScores.playerName;
+        userScore = PlayerScores.playerScore;
+    }
+}
